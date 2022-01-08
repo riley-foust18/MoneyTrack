e59a38dc8fbce5cd20e8e489751bd5e7a7290476
DoodleBob <riley.foust18@gmail.com>
2022-01-08 16:03:17 -0500

    created offline data saved to indexdb

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,2 +1,35 @@
 let db;
 const request = indexedDB.open('money_track', 1);
+
+request.onupgradeneeded = function(event) {
+  const db = event.target.result;
+  db.createObjectStore('new_input', {autoIncrement: true});
+}
+
+request.onsuccess = function(event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    uploadTransaction();
+  };
+};
+
+request.onerror = function(event) {
+  console.log(event.target.errorCode);
+}
+
+function saveRecord(record) {
+  const transaction = db.transaction(['new_input'], 'readwrite');
+
+  const moneyObjectStore = transaction.objectStore('new_input');
+
+  moneyObjectStore.add(record);
+}
+
+function uploadTransaction() {
+  const transaction = db.transaction(['new_input'], 'readwrite');
+
+  const moneyObjectStore = transaction.objectStore('new_input');
+
+  const getAll = moneyObjectStore.getAll();
+}
